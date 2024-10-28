a3ed0a2be304357bd027f59280fc812d21ad2d34
rnnae <115386512+rnnae@users.noreply.github.com>
2024-10-28 20:00:17 -0300

    Update main.c
    Adiciona sleep();

--- jan-ken-po/main.c
+++ jan-ken-po/main.c
@@ -1,27 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#ifdef _WIN32
+    #include <windows.h>
+#else
+    #include <unistd.h>
+#endif
 
 void jogarJokenpo() {
-    int escolha_usuario, escolha_maquina;
+    int escolha_usuario, escolha_maquina, pausa;
     char *opcoes[] = {"Pedra", "Papel", "Tesoura"};
 
     printf("\nEscolha entre Pedra (0), Papel (1) ou Tesoura (2)\n");
     printf("Digite sua escolha: ");
     scanf("%d", &escolha_usuario);
 
-    // Validação da entrada do usuário
     if (escolha_usuario < 0 || escolha_usuario > 2) {
         printf("Escolha inválida! Tente novamente.\n");
         return;
     }
 
-    // Gera escolha aleatória da máquina
-    escolha_maquina = rand() % 3;
     printf("\nVocê escolheu: %s\n", opcoes[escolha_usuario]);
+    for (int i = 0; i < 3; i++) {
+        printf(".");
+        fflush(stdout);
+        #ifdef _WIN32
+            Sleep(1000);
+        #else
+            sleep(1);
+        #endif
+    }
+    printf("\n");
+    escolha_maquina = rand() % 3;
     printf("A máquina escolheu: %s\n", opcoes[escolha_maquina]);
+    #ifdef _WIN32
+        Sleep(1000);
+    #else
+        sleep(1);
+    #endif
 
-    // Determina o resultado
     if (escolha_usuario == escolha_maquina) {
         printf("Empate ._.\n");
     } else if ((escolha_usuario == 0 && escolha_maquina == 2) || (escolha_usuario == 1 && escolha_maquina == 0) || (escolha_usuario == 2 && escolha_maquina == 1)) {
@@ -34,9 +51,8 @@ void jogarJokenpo() {
 int main() {
     int jogarNovamente;
 
-    // Inicializa o gerador de números aleatórios
     srand(time(NULL));
-
+    
     printf("Bem-vindo ao Jokenpô contra o rand (heheh)!\n");
 
     do {
