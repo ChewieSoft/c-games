629950e7f82c44233aacf13d573a9bcd09943c4e
j0ruge <joruge@gmail.com>
2024-11-03 16:21:33 -0300

    update para aula

--- _s_n_a_k_e_/app.c
+++ _s_n_a_k_e_/app.c
@@ -67,9 +67,9 @@ void draw_board()
             else
             {
                 int is_tail_part = false;
-                for (int k = 0; k < snake_tail_length; k++)
+                for (int tail_segment_index = 0; tail_segment_index < snake_tail_length; tail_segment_index++)
                 {
-                    if (snake_tail_x[k] == column && snake_tail_y[k] == row)
+                    if (snake_tail_x[tail_segment_index] == column && snake_tail_y[tail_segment_index] == row)
                     {
                         printf("%so%s", set_print(fgGREEN, bgBLACK, caNORMAL), RESET);
                         is_tail_part = true;
@@ -94,8 +94,7 @@ void draw_board()
     }        
     printf("\n");
 
-    printf("Score: %d\n", score);
-    printf("x: %i, y: %i", snake_head_x, snake_head_y);
+    printf("\n%sScore: %s%d%s\n", CYAN, YELLOW, score, RESET);    
 }
 
 // Captura a entrada do usuário
@@ -121,56 +120,58 @@ void capture_input() {
     }
 }
 
-// Atualiza a lógica da cobra e da fruta
 void update_game_logic()
 {
-    int previous_x = snake_tail_x[0];
-    int previous_y = snake_tail_y[0];
-    int temp_x, temp_y;
-    snake_tail_x[0] = snake_head_x;
-    snake_tail_y[0] = snake_head_y;
+    // Movimenta a cabeça da cobra de acordo com a direção
+    int previous_x = snake_head_x;
+    int previous_y = snake_head_y;
 
-    // Atualiza posição da cauda
-    for (int i = 1; i < snake_tail_length; i++)
+    switch (direction)
     {
-        temp_x = snake_tail_x[i];
-        temp_y = snake_tail_y[i];
-        snake_tail_x[i] = previous_x;
-        snake_tail_y[i] = previous_y;
+    case LEFT:
+        snake_head_x--;
+        break;
+    case RIGHT:
+        snake_head_x++;
+        break;
+    case UP:
+        snake_head_y--;
+        break;
+    case DOWN:
+        snake_head_y++;
+        break;
+    default:
+        break;
+    }
+
+    // Aplica wrap-around nas bordas para a cabeça antes de atualizar a cauda
+    if (snake_head_x >= BOARD_WIDTH) snake_head_x = 0;
+    else if (snake_head_x < 0) snake_head_x = BOARD_WIDTH - 1;
+
+    if (snake_head_y >= BOARD_HEIGHT) snake_head_y = 0;
+    else if (snake_head_y < 0) snake_head_y = BOARD_HEIGHT - 1;
+
+    // Movimenta cada segmento da cauda para a posição do segmento anterior
+    int temp_x, temp_y;
+    for (int tail_segment_index = 0; tail_segment_index < snake_tail_length; tail_segment_index++)
+    {
+        temp_x = snake_tail_x[tail_segment_index];
+        temp_y = snake_tail_y[tail_segment_index];
+        snake_tail_x[tail_segment_index] = previous_x;
+        snake_tail_y[tail_segment_index] = previous_y;
         previous_x = temp_x;
         previous_y = temp_y;
     }
 
-    // Movimenta a cabeça da cobra
-    switch (direction)
-    {
-        case LEFT:
-            snake_head_x--;
-            break;
-        case RIGHT:
-            snake_head_x++;
-            break;
-        case UP:
-            snake_head_y--;
-            break;
-        case DOWN:
-            snake_head_y++;
-            break;
-        default:
-            break;
-    }
-
-    // Wrap-around nas bordas
-    if (snake_head_x >= BOARD_WIDTH) snake_head_x = 0;
-    else if (snake_head_x < 0) snake_head_x = BOARD_WIDTH - 1;
-    if (snake_head_y >= BOARD_HEIGHT) snake_head_y = 0;
-    else if (snake_head_y < 0) snake_head_y = BOARD_HEIGHT - 1;
-
-    // Verifica colisão com a cauda
+    // Verifica colisão da cabeça com a cauda
     for (int tail_index = 0; tail_index < snake_tail_length; tail_index++)
     {
         if (snake_tail_x[tail_index] == snake_head_x && snake_tail_y[tail_index] == snake_head_y)
+        {
+            printf("\n%s> %s %sCollision detected with the tail!%s\n", GREEN, eCROSS, set_print(fgRED, bgBLACK, caNORMAL), RESET);
+			printf("%s> %sGAME OVER!%s\n", GREEN, set_print(fgRED, bgBLACK, caBLINKING), RESET);
             is_game_over = true;
+        }
     }
 
     // Verifica se a cobra comeu a fruta
@@ -180,8 +181,15 @@ void update_game_logic()
         fruit_x = rand() % BOARD_WIDTH;
         fruit_y = rand() % BOARD_HEIGHT;
         snake_tail_length++;
+        printf("Fruit eaten! New fruit position: (%d, %d)\n", fruit_x, fruit_y);
     }
+
+    // Log de posição para debug
+    printf("\nLog\n");
+    printf("Head position: (%d, %d), Tail length: %d\n", snake_head_x, snake_head_y, snake_tail_length);
 }
+
+
 
 // Função principal do jogo
 int main()
